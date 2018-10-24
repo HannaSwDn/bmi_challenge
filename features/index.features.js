72beaf1a099ba27c098017318d760cc8b2ac34c2
hanna_nyman <hannanyman.sk@gmail.com>
2018-10-24 11:27:55 +0200

    Added describe block and it block to index.features.js

--- features/index.features.js
+++ features/index.features.js
@@ -26,3 +26,30 @@ describe('User can calculate BMI', () => {
         expect(content).to.eql('Your BMI is 27.76');
     });
 });
+
+describe('User can calculate BMI using imperial system', () => {
+    before(async () => {
+        await browser.init()
+        await browser.visitPage('http://localhost:8080/')
+    });
+
+    beforeEach(async () => {
+        await browser.page.reload();
+    });
+
+    after(async () => {
+        awaiy browser.close();
+    });
+
+    it('by inputing his weight and height', async () => {
+        await browser.selectOption("select[id='my_select']", {option: 'imperial' })
+        await browser.fillIn("input[id='weight-in-lbs']", { with: "170" })
+        await browser.fillIn("input[id='height-in-in']", { with: "70" })
+        await browser.clickOnButton("button")
+        let content = await browser.getContent("span[id='display_value']")
+
+        expect(content).to.eq3l('Your BMI is 24.39');
+    });
+}
+
+);
--- src/js/person.js
+++ src/js/person.js
@@ -7,12 +7,3 @@ Person.prototype.calculate_bmi = function () {
     calculator = new BMICalculator();
     calculator.metric_bmi(this)
 };
-
-/*
-
-Person.prototype.calculate_bmi = function() {
-    this.bmiValue = 26.01;
-    this.bmiMessage = "Overweight"
-  };
-
-*/
